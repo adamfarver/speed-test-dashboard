9c315560b39ebdb48a8132187d4eb3a4b925e6d7
Adam Farver <adamfarver@gmail.com>
2020-04-20 15:06:00 -0400

    All aggregations done.

--- index.js
+++ index.js
@@ -1,43 +1,41 @@
 'use strict'
-const express = require("express")
-const speedTest = require("speedtest-net");
-const Data = require("./models/testData")
+const express = require('express')
+const speedTest = require('speedtest-net')
+const Data = require('./models/testData')
 const mongoose = require('mongoose')
-const cron = require("node-cron")
-const routes = require("./routes/index")
+const cron = require('node-cron')
+const routes = require('./routes/index')
 
-mongoose.connect('mongodb://localhost:27017/ispTestData', { useNewUrlParser: true });
+mongoose.connect('mongodb://localhost:27017/ispTestData', {
+	useNewUrlParser: true,
+})
 
-const app = express();
+const app = express()
 //Offline puts more than one entry in db
 let outputCount = 0
-app.use("/", routes)
+app.use('/', routes)
 cron.schedule('*/15 * * * *', () => {
-  let runDate = new Date()
+	let runDate = new Date()
 
-  console.log("Running Test", runDate.toString())
-  const test = speedTest({ maxTime: 5000 });
-  // When test results finish, save to db.
-  test.on("data", data => {
-    if (!outputCount) {
-      const constructedData = new Data({ date: runDate, data });
-      constructedData.save().then(() => console.log(data))
+	console.log('Running Test', runDate.toString())
+	const test = speedTest({ maxTime: 5000 })
+	// When test results finish, save to db.
+	test.on('data', (data) => {
+		if (!outputCount) {
+			const constructedData = new Data({ date: runDate, data })
+			constructedData.save().then(() => console.log(data))
+		}
+	})
 
-    }
-  });
-
-  test.on("error", err => {
-    if (!outputCount) {
-      const constructedData = new Data({ date: runDate, data: err });
-      constructedData.save().then(() => console.log("Saved"))
-      console.log("Offline: ", new Date());
-      outputCount++
-    }
-  });
-
-
-});
+	test.on('error', (err) => {
+		if (!outputCount) {
+			const constructedData = new Data({ date: runDate, data: err })
+			constructedData.save().then(() => console.log('Saved'))
+			console.log('Offline: ', new Date())
+			outputCount++
+		}
+	})
+})
 
 const port = 5000
-app.listen(port, () => console.log("Webserver listening on ", port))
-
+app.listen(port, () => console.log('Webserver listening on ', port))
--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,79 @@
+/**
+ * @format
+ */
+'use strict'
 const express = require('express')
 const router = express.Router()
 const mongoose = require('mongoose')
-const model = require("../models/testData")
+const model = require('../models/testData')
 
+router.get('/', async (req, res) => {
+	const data = {}
+	//get last test
+	const lastTest = await model.aggregate([
+		{ $sort: { date: -1 } },
+		{ $limit: 1 },
+		{
+			$group: {
+				_id: null,
+				DL: { $avg: '$data.speeds.download' },
+				UL: { $avg: '$data.speeds.upload' },
+				ping: { $avg: '$data.server.ping' },
+			},
+		},
+		{ $project: { _id: 0, DL: 1, UL: 1, ping: 1 } },
+	])
+	data.lastTest = lastTest[0]
+	// get last hour
+	const lastHour = await model.aggregate([
+		{ $sort: { date: -1 } },
+		{ $limit: 4 },
+		{
+			$group: {
+				_id: null,
+				averageDL: { $avg: '$data.speeds.download' },
+				averageUL: { $avg: '$data.speeds.upload' },
+				averagePing: { $avg: '$data.server.ping' },
+			},
+		},
+		{ $project: { _id: 0, averageDL: 1, averageUL: 1, averagePing: 1 } },
+	])
+	data.lastHour = lastHour[0]
 
-//view results
-router.get("/", async function (req, res, next) {
-	const data = await model.find({ "data.speeds.download": { $gt: 10 } })
-	console.log(data)
+	//get last day
+	const lastDay = await model.aggregate([
+		{ $sort: { date: -1 } },
+		{ $limit: 96 },
+		{
+			$group: {
+				_id: null,
+				averageDL: { $avg: '$data.speeds.download' },
+				averageUL: { $avg: '$data.speeds.upload' },
+				averagePing: { $avg: '$data.server.ping' },
+			},
+		},
+		{ $project: { _id: 0, averageDL: 1, averageUL: 1, averagePing: 1 } },
+	])
+	data.lastDay = lastDay[0]
+
+	//get last week
+
+	const lastWeek = await model.aggregate([
+		{ $sort: { date: -1 } },
+		{ $limit: 672 },
+		{
+			$group: {
+				_id: null,
+				averageDL: { $avg: '$data.speeds.download' },
+				averageUL: { $avg: '$data.speeds.upload' },
+				averagePing: { $avg: '$data.server.ping' },
+			},
+		},
+		{ $project: { _id: 0, averageDL: 1, averageUL: 1, averagePing: 1 } },
+	])
+	data.lastWeek = lastWeek[0]
+
 	res.send(data)
 })
 
-
-
-
-
 module.exports = router
-
-
